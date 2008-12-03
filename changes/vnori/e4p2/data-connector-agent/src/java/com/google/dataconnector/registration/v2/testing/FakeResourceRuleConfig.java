6d8d8371ecb920e9d99f6ab4e306472442b6c9fe
vasunori <vasunori@466b8f56-9b98-11dd-b1d9-6d7c01a26949>
2008-12-03 23:21:12 +0000

    fixing a bug associated with Client hanging around even when tunnelserver is gone.
    and removing the source-distro, binary-distro dependencies from the ant default target 'all'

---
+++ changes/vnori/e4p2/data-connector-agent/src/java/com/google/dataconnector/registration/v2/testing/FakeResourceRuleConfig.java
@@ -0,0 +1,183 @@
+/* Copyright 2008 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.google.dataconnector.registration.v2.testing;
+
+import com.google.dataconnector.registration.v2.ResourceRule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility that provides pre populated registration configuration for testing purposes.
+ *
+ * @author rayc@google.com (Ray Colline)
+ */
+public class FakeResourceRuleConfig {
+
+  // Resource Values
+  public static final String CLIENT_ID = "all";
+  public static final String HTTP_NAME = "1";
+  public static final String[] ALLOWED_ENTITY = 
+      { "rcolline@test.joonix.net", "admin@test.joonix.net" };
+  public static final String[] APPID = { "someappid", "someappid2" };
+  public static final String HTTP_PATTERN = "http://www.example.com";
+  public static final String SOCKET_NAME = "2";;
+  public static final String SOCKET_PATTERN = "socket://128.195.131";
+  public static final String HTTP_PROXY_PORT = "19233";
+  public static final String SOCKS_SERVER_PORT = "1080";
+  public static final String SECRET_KEY = "23423432432";
+
+  /** The fake properties file we generate the config from */
+  private List<ResourceRule> configResourceRules;
+  private List<ResourceRule> runtimeResourceRules;
+  private ResourceRule runtimeHttpResourceRule;
+  private ResourceRule runtimeSocketResourceRule;
+  private ResourceRule configHttpResourceRule;
+  private ResourceRule configSocketResourceRule;
+
+  /**
+   * Creates a configuration beans from the fake hardcoded XML files.  
+   */
+  public FakeResourceRuleConfig() {
+
+    // Configure each resource rule to match XML.
+    runtimeHttpResourceRule = getBaseResourceRule();
+    runtimeHttpResourceRule.setPattern(HTTP_PATTERN);
+    runtimeHttpResourceRule.setName(HTTP_NAME);
+    runtimeHttpResourceRule.setSecretKey(Long.valueOf(SECRET_KEY));
+    runtimeHttpResourceRule.setHttpProxyPort(Integer.valueOf(HTTP_PROXY_PORT));
+    runtimeHttpResourceRule.setSocksServerPort(Integer.valueOf(SOCKS_SERVER_PORT));
+    runtimeHttpResourceRule.setAppIds(APPID);
+    runtimeSocketResourceRule = getBaseResourceRule();
+    runtimeSocketResourceRule.setPattern(SOCKET_PATTERN);
+    runtimeSocketResourceRule.setName(SOCKET_NAME);
+    runtimeSocketResourceRule.setSecretKey(Long.valueOf(SECRET_KEY));
+    runtimeSocketResourceRule.setSocksServerPort(Integer.valueOf(SOCKS_SERVER_PORT));
+    configHttpResourceRule = getBaseResourceRule();
+    configHttpResourceRule.setPattern(HTTP_PATTERN);
+    configHttpResourceRule.setName(HTTP_NAME);
+    configHttpResourceRule.setAppIds(APPID);
+    configSocketResourceRule = getBaseResourceRule();
+    configSocketResourceRule.setPattern(SOCKET_PATTERN);
+    configSocketResourceRule.setName(SOCKET_NAME);
+    
+    // Add to Lists
+    configResourceRules = new ArrayList<ResourceRule>();
+    configResourceRules.add(configHttpResourceRule);
+    configResourceRules.add(configSocketResourceRule);
+    runtimeResourceRules = new ArrayList<ResourceRule>();
+    runtimeResourceRules.add(runtimeHttpResourceRule);
+    runtimeResourceRules.add(runtimeSocketResourceRule);
+  }
+
+  /**
+   * @return list of ResourceRule populated with a fake set of configuration from 
+   * {@link #CONFIG_RESOURCE_RULES_XML}
+   */
+  public List<ResourceRule> getFakeConfigResourceRules() {
+    return configResourceRules;
+  }
+  
+  /**
+   * @return list of ResourceRule populated with a fake set of configuration from 
+   * {@link #CONFIG_RESOURCE_RULES_XML}
+   */
+  public List<ResourceRule> getFakeRuntimeResourceRules() {
+    return runtimeResourceRules;
+  }
+  
+  public ResourceRule getRuntimeHttpResourceRule() {
+    return runtimeHttpResourceRule;
+  }
+
+  public ResourceRule getRuntimeSocketResourceRule() {
+    return runtimeSocketResourceRule;
+  }
+
+  public ResourceRule getConfigHttpResourceRule() {
+    return configHttpResourceRule;
+  }
+
+  public ResourceRule getConfigSocketResourceRule() {
+    return configSocketResourceRule;
+  }
+  
+  private ResourceRule getBaseResourceRule() {
+    ResourceRule resourceRule = new ResourceRule();
+    resourceRule.setClientId(CLIENT_ID);
+    resourceRule.setAllowedEntities(ALLOWED_ENTITY);
+    return resourceRule;
+  }
+  
+
+  public static final String CONFIG_RESOURCE_RULES_XML = "<feed>\n" +
+    "<entity repeatable='true'>\n" +
+    "<name>" + HTTP_NAME + "</name>\n" +
+    "<clientId>" + CLIENT_ID + "</clientId>\n" +
+    "<allowedEntities repeatable='true'>" + ALLOWED_ENTITY[0] + "</allowedEntities>\n" +
+    "<allowedEntities>" + ALLOWED_ENTITY[1] + "</allowedEntities>\n" +
+    "<appIds repeatable='true'>" + APPID[0] + "</appIds>\n" +
+    "<appIds>" + APPID[1] + "</appIds>\n" +
+    "<pattern>" + HTTP_PATTERN + "</pattern>\n" +
+    "</entity>\n" +
+    "<entity>\n" +
+    "<name>" + SOCKET_NAME + "</name>\n" +
+    "<clientId>" + CLIENT_ID + "</clientId>\n" +
+    "<allowedEntities repeatable='true'>" + ALLOWED_ENTITY[0] + "</allowedEntities>\n" +
+    "<allowedEntities>" + ALLOWED_ENTITY[1] + "</allowedEntities>\n" +
+    "<pattern>" + SOCKET_PATTERN + "</pattern>\n" +
+    "</entity>\n" +
+    "</feed>\n" ;
+  
+  public static final String RUNTIME_RESOURCE_RULES_XML = "<feed>\n" +
+    "<entity repeatable='true'>\n" +
+    "<name>" + HTTP_NAME + "</name>\n" +
+    "<clientId>" + CLIENT_ID + "</clientId>\n" +
+    "<allowedEntities repeatable='true'>" + ALLOWED_ENTITY[0] + "</allowedEntities>\n" +
+    "<allowedEntities>" + ALLOWED_ENTITY[1] + "</allowedEntities>\n" +
+    "<appIds repeatable='true'>" + APPID[0] + "</appIds>\n" +
+    "<appIds>" + APPID[1] + "</appIds>\n" +
+    "<pattern>" + HTTP_PATTERN + "</pattern>\n" +
+    "<httpProxyPort>" + HTTP_PROXY_PORT + "</httpProxyPort>\n" +
+    "<socksServerPort>" + SOCKS_SERVER_PORT + "</socksServerPort>\n" +
+    "<secretKey>" + SECRET_KEY +"</secretKey>\n" +
+    "</entity>\n" +
+    "<entity>\n" +
+    "<name>" + SOCKET_NAME + "</name>\n" +
+    "<clientId>" + CLIENT_ID + "</clientId>\n" +
+    "<allowedEntities repeatable='true'>" + ALLOWED_ENTITY[0] + "</allowedEntities>\n" +
+    "<allowedEntities>" + ALLOWED_ENTITY[1] + "</allowedEntities>\n" +
+    "<pattern>" + SOCKET_PATTERN + "</pattern>\n" +
+    "<socksServerPort>" + SOCKS_SERVER_PORT + "</socksServerPort>\n" +
+    "<secretKey>" + SECRET_KEY +"</secretKey>\n" +
+    "</entity>\n" +
+    "</feed>\n" ;
+  
+  public static final String RUNTIME_RESOURCE_ENTITY_XML =
+    "<entity repeatable='true'>\n" +
+    "<name>" + HTTP_NAME + "</name>\n" +
+    "<clientId>" + CLIENT_ID + "</clientId>\n" +
+    "<allowedEntities repeatable='true'>" + ALLOWED_ENTITY[0] + "</allowedEntities>\n" +
+    "<allowedEntities>" + ALLOWED_ENTITY[1] + "</allowedEntities>\n" +
+    "<appIds repeatable='true'>" + APPID[0] + "</appIds>\n" +
+    "<appIds>" + APPID[1] + "</appIds>\n" +
+    "<pattern>" + HTTP_PATTERN + "</pattern>\n" +
+    "<httpProxyPort>" + HTTP_PROXY_PORT + "</httpProxyPort>\n" +
+    "<socksServerPort>" + SOCKS_SERVER_PORT + "</socksServerPort>\n" +
+    "<secretKey>" + SECRET_KEY +"</secretKey>\n" +
+    "</entity>\n";
+}
---
+++ changes/vnori/e4p2/data-connector-agent/src/java/com/google/dataconnector/util/LocalConfValidator.java
@@ -0,0 +1,230 @@
+/* Copyright 2008 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.google.dataconnector.util;
+
+import java.io.File;
+
+/**
+ * Validates the local configuration bean.
+ * 
+ * @author rayc@google.com (Ray Colline)
+ */
+public class LocalConfValidator {
+  
+  private FileFactory fileFactory;
+  
+  /**
+   * Builds a validator with pre-configured dependencies.
+   */
+  public LocalConfValidator() {
+    this(new FileFactory());
+  }
+  
+  /**
+   * Constructs the validator with dependencies injected.
+   * 
+   * @param fileFactory the factory used to get new {@link File} instances.
+   */
+  public LocalConfValidator(FileFactory fileFactory) {
+    this.fileFactory = fileFactory;
+  }
+  
+  private static final int MAX_PORT = 65535;
+
+  /**
+   * With the supplied filename, this method calls {@link File#canRead()}.
+   * 
+   * @param configKey the config file key associated with the supplied file name.
+   * @param filename the filename to check for read access
+   * @throws LocalConfException if {@link File#canRead()} fails and will contain a message
+   * associated the file with the supplied config file key.
+   */
+  private String canReadFile(String configKey, String filename) throws LocalConfException {
+    File file = fileFactory.getFile(filename);
+    if (!file.canRead()) {
+      return("Cannot read " + configKey + "file: " + filename + "\n");
+    }
+    return "";
+  }
+  
+  /**
+   * Validates the LocalConf bean using a series of semantic checks.
+   * 
+   * @param localConf the bean to validate.
+   * @throws LocalConfException if any config entry is invalid.  This code batches up
+   * all errors and throws them at once for improved user experience.
+   */
+  public void validate(LocalConf localConf) throws LocalConfException {
+    
+    StringBuilder errors = new StringBuilder();
+    
+    // rulesFile
+    String rulesFile = localConf.getRulesFile();
+    if (rulesFile != null) {
+      errors.append(canReadFile("rulesFile", rulesFile));
+    } else {
+      errors.append("'rulesFile' required\n");
+    }
+    
+    // sdcServerHost
+    if (localConf.getSdcServerHost() == null) {
+      errors.append("'sdcServerHost' required\n");
+    }
+    
+    // sdcServerPort
+    Integer sdcServerPort = localConf.getSdcServerPort();
+    if (sdcServerPort != null) {
+      if (sdcServerPort > MAX_PORT || sdcServerPort < 0) {
+        errors.append("invalid 'sdcServerPort': " + sdcServerPort);
+      }
+    } else {
+      errors.append("'sdcServerPort' required\n");
+    }
+    
+    // domain
+    if (localConf.getDomain() != null) {
+      /*
+       * Regex verifies the following.  Credit: 
+       * http://www.martienus.com/code/regex-validate-e-mail-address-in-php.html
+       * 
+       * Sub-domain name may only contain letters, digits and hyphen
+       * Multiple sub-domains are permitted
+       * Domain name may only contain letters, digits and hyphen
+       * Domain name and sub-domain name may not begin and/or end with a hyphen
+       * Domain name and sub-domain must be between 2 to 63 characters long
+       * Top-level domain may only contain letters
+       * Top-level domain must be between 2 to 6 characters long
+       * Sub-domain names, the domain name and the top-level domain name are separated by 
+       * single periods '.'
+       */
+      if (!localConf.getDomain().matches("^(([A-z0-9]+\\-?[A-z0-9]+)+\\.)+[A-z]{2,6}$")) {
+        errors.append("'domain' " + localConf.getDomain() + " not valid\n");
+      }
+    } else {
+      errors.append("'domain' required\n");
+    }
+    
+    // user
+    if (localConf.getUser() != null) {
+      if (localConf.getUser().matches("\\s")) {
+        errors.append("'user' contains spaces\n");
+      }
+    } else {
+      errors.append("'user' required\n");
+    }
+    
+    // oauthKey
+    if (localConf.getOauthKey() == null) {
+      errors.append("'oauthKey' required\n");
+    }
+    
+    // useSsl
+    if (localConf.getUseSsl() == null) {
+      errors.append("'useSsl' required\n");
+    }
+    
+    // sslKeyStoreFile
+    if (localConf.getSslKeyStoreFile() != null) {
+      errors.append(canReadFile("sslKeyStoreFile", localConf.getSslKeyStoreFile()));
+    } 
+    
+    // sslKeyStorePassword 
+    if (localConf.getSslKeyStoreFile() != null && localConf.getSslKeyStorePassword() == null) {
+        errors.append("'sslKeyStorePassword' required\n");
+    }
+    
+    // clientId
+    if (localConf.getClientId() != null) {
+      if (localConf.getClientId().matches("\\s")) {
+        errors.append("'clientId' contains spaces\n");
+      }
+    } else {
+      errors.append("'clientId' required\n");
+    }
+    
+    // sshd
+    if (localConf.getSshd() != null) {
+      errors.append(canReadFile("sshd", localConf.getSshd()));
+    } else {
+      errors.append("'sshd' required\n");
+    }
+    
+    // startingHttpProxyPort 
+    Integer startingHttpProxyPort = localConf.getStartingHttpProxyPort();
+    if (startingHttpProxyPort != null) {
+      if (startingHttpProxyPort > MAX_PORT || startingHttpProxyPort < 0) {
+        errors.append("invalid 'startingHttpProxyPort': " +  startingHttpProxyPort);
+      }
+    } else {
+      errors.append("'startingHttpProxyPort' required\n");
+    }
+    
+    // httpProxyBindHost
+    if (localConf.getHttpProxyBindHost() != null) {
+      if (localConf.getHttpProxyBindHost().matches("\\s")) {
+        errors.append("'httpProxyBindHost' contains spaces\n");
+      }
+    } else {
+      errors.append("'httpProxyBindHost' required\n");
+    }
+    
+    // socksServerPort 
+    Integer socksServerPort = localConf.getSocksServerPort();
+    if (socksServerPort != null) {
+      if (socksServerPort > MAX_PORT || socksServerPort < 0) {
+        errors.append("invalid 'socksServerPort': " +  socksServerPort);
+      }
+    } else {
+      errors.append("'socksServerPort' required\n");
+    }
+    
+    // sockdBindHost
+    if (localConf.getSocksdBindHost() != null) {
+      if (localConf.getSocksdBindHost().matches("\\s")) {
+        errors.append("'socksdBindHost' contains spaces\n");
+      }
+    } else {
+      errors.append("'socksdBindHost' required\n");
+    }
+    
+    // logProperties 
+    if (localConf.getLogProperties() == null) {
+      errors.append("'logProperties' required\n");
+    }
+    
+    // socksProperties 
+    if (localConf.getSocksProperties() == null) {
+      errors.append("'socksProperties' required\n");
+    }
+    
+    // Check for errors and throw
+    if (errors.length() > 0) {
+      throw new LocalConfException(errors.toString());
+    }
+  }
+  
+  /**
+   * Factory so we can dependency inject the File object.
+   * 
+   * @author rayc@google.com (Ray Colline)
+   */
+  public static class FileFactory {
+    public File getFile(String fileName) {
+      return new File(fileName);
+    }
+  }
+}

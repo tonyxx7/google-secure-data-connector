4d4c2906395fab29426ca9bb88b88750c2eb7c3c
vasunori <vasunori@466b8f56-9b98-11dd-b1d9-6d7c01a26949>
2008-12-09 20:23:53 +0000

    fix the broken unittest

---
+++ changes/vnori/b8v5/data-connector-agent/src/java/com/google/dataconnector/registration/v1/ResourceException.java
@@ -0,0 +1,43 @@
+/* Copyright 2008 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.google.dataconnector.registration.v1;
+
+/**
+ * Exception for errors during protocol communications with Secure Link Server.
+ * 
+ * @author rayc@google.com (Ray Colline)
+ */
+public class ResourceException extends Exception {
+
+  /**
+   * Creates the exception with the specified error message.
+   * 
+   * @param msg the error message.
+   */
+  public ResourceException(String msg) {
+    super(msg);
+  }
+  
+  
+  public ResourceException(Throwable cause) {
+    super(cause);
+  }
+  
+  public ResourceException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}

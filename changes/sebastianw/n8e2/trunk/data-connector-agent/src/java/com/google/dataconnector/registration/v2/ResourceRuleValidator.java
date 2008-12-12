1c419e994654e93b5af499153f656d9d313bcce1
sebastian.welsh <sebastian.welsh@466b8f56-9b98-11dd-b1d9-6d7c01a26949>
2008-12-12 09:10:16 +0000

    Per Ray's suggestion, scrubbed a whole bunch of the debian-specific configs. Instead, do post-install munging of the templates. Still using compiled rather than system httpd.

---
+++ changes/sebastianw/n8e2/trunk/data-connector-agent/src/java/com/google/dataconnector/registration/v2/ResourceRuleValidator.java
@@ -0,0 +1,176 @@
+/* Copyright 2008 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.google.dataconnector.registration.v2;
+
+import java.util.List;
+
+/**
+ * Validates a resource rule and produces errors.
+ * 
+ * @author rayc@google.com (Ray Colline)
+ */
+public class ResourceRuleValidator {
+  
+  private static final int MAX_PORT = 65535;
+  
+  /**
+   * Iterates through the provided list of resource rules and validates each one against the
+   * required runtime configuration.
+   * 
+   * @param resourceRules a list of resource rules
+   * @throws ResourceException if there are any invalid rules.  The message contained in
+   * the exception will indicate the error.
+   */
+  public void validateRuntime(List<ResourceRule> resourceRules) 
+      throws ResourceException {
+    for (ResourceRule resourceRule : resourceRules) {
+      validateRuntime(resourceRule);
+    }
+  }
+  
+  /**
+   * Iterates through the provided list of resource rules and validates each one against the
+   * required configuration.
+   * 
+   * @param resourceRules a list of resource rules
+   * @throws ResourceException if there are any invalid rules.  The message contained in
+   * the exception will indicate the error.
+   */
+  public void validate(List<ResourceRule> resourceRules) 
+      throws ResourceException {
+    for (ResourceRule resourceRule : resourceRules) {
+      validate(resourceRule);
+    }
+  }
+  
+  /**
+   * Validates a single resource rule against the required runtime configuration.  
+   * 
+   * @param resourceRule a resource rule configured for runtime usage.
+   * @throws ResourceException if there are any invalid rules.  The message contained in
+   * the exception will indicate the error.
+   */
+  public void validateRuntime(ResourceRule resourceRule) 
+      throws ResourceException {
+    
+    // All Config time validation and ...
+    validate(resourceRule);
+    
+    // httpProxyPort - required for all HTTP resources
+    // TODO(rayc) remove when we eliminate wpg-proxy
+    Integer httpProxyPort = resourceRule.getHttpProxyPort();
+    if (httpProxyPort != null) {
+      if (httpProxyPort > MAX_PORT || httpProxyPort < 0) {
+        throw new ResourceException("HttpProxyPort " + httpProxyPort + " out of range.");
+      }
+    } else if (resourceRule.getPattern().trim().startsWith(ResourceRule.HTTPID)) {
+      throw new ResourceException("'httpProxyPort' required for each " + ResourceRule.HTTPID +
+          "resource");
+    }
+    
+    // socksServerPort 
+    Integer socksServerPort = resourceRule.getSocksServerPort();
+    if (socksServerPort != null) {
+      if (socksServerPort > MAX_PORT || socksServerPort < 0) {
+        throw new ResourceException("socksServerPort " + socksServerPort + " out of range.");
+      }
+    } else {
+      throw new ResourceException("'socksServerPort' required for each resource");
+    }
+    
+    // secretKey
+    if (resourceRule.getSecretKey() == null) {
+      throw new ResourceException("Rule is missing secret key");
+    }
+  }
+  
+  /**
+   * Validates a single resource rule against the required configuration.  
+   * 
+   * @param resourceRule a resource rule at configuration time.
+   * @throws ResourceException if there are any invalid rules.  The message contained in
+   * the exception will indicate the error.
+   */
+  public void validate(ResourceRule resourceRule) throws ResourceException {
+    
+    // Name 
+    if (resourceRule.getName() != null) {
+      try {
+        Integer.valueOf(resourceRule.getName().trim());
+       } catch (NumberFormatException e) {
+         throw new ResourceException("'name' field must be a valid integer", e);
+       }
+    } else {
+      throw new ResourceException("'name' field must be present");
+    }
+    
+    // clientId
+    if (resourceRule.getClientId() != null) {
+       if (resourceRule.getClientId().trim().contains(" ")) {
+         throw new ResourceException("'clientId' field " + resourceRule.getClientId() + 
+             " must not contain any white space.");
+       }
+    } else { 
+      throw new ResourceException("'clientId' field must be present");
+    }
+    
+    // allowed entities
+    if (resourceRule.getAllowedEntities() != null) {
+      for (String allowedEntity : resourceRule.getAllowedEntities()) {
+        if (allowedEntity.trim().contains(" ")) {
+           throw new ResourceException(
+               "'allowedEntities' field " + allowedEntity + " must not contain any white space.");
+        }
+        if (!allowedEntity.trim().contains("@")) {
+           throw new ResourceException(
+               "'allowedEntities' field " + allowedEntity + " must be a valid fully qualified " +
+                     "email address");
+          
+        }
+      }
+    } else {
+      throw new ResourceException("at least one 'allowedEntities' field must be present");
+    }
+    
+    // appids - not required
+    if (resourceRule.getAppIds() != null) {
+      for (String appId : resourceRule.getAppIds()) {
+        if (appId.trim().contains(" ")) {
+           throw new ResourceException(
+               "'appIds' field " + appId + " must not contain any white space.");
+        }
+      }
+    } 
+    
+    // pattern
+    String pattern = resourceRule.getPattern();
+    if (pattern != null) {
+      pattern = pattern.trim();
+      if (pattern.contains(" ")) {
+        throw new ResourceException("'pattern' field " + pattern + 
+            " must not contain any white space.");
+      }
+      if (!pattern.startsWith(ResourceRule.HTTPID) && 
+          !pattern.startsWith(ResourceRule.HTTPSID) && 
+          !pattern.startsWith(ResourceRule.SOCKETID)) {
+        throw new ResourceException("Invalid pattern, missing identifier: " + pattern);
+      }
+    } else {
+      throw new ResourceException("'pattern' must be present.");
+    }
+  }
+}

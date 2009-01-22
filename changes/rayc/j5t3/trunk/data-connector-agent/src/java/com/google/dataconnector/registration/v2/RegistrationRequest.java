6750e062eb3fd853984e562f2180ef145b661a43
raycolline <raycolline@466b8f56-9b98-11dd-b1d9-6d7c01a26949>
2009-01-22 21:46:55 +0000

    Bug 1520448   - now checks all file permissions at startup and will bail if they are incorrect.     - this may be too pedantic but i think it will help customers debug issues quicker   - fixed various file permissions that were off (Yay TDD)

---
+++ changes/rayc/j5t3/trunk/data-connector-agent/src/java/com/google/dataconnector/registration/v2/RegistrationRequest.java
@@ -0,0 +1,100 @@
+/* Copyright 2008 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.google.dataconnector.registration.v2;
+
+import com.google.dataconnector.registration.v2.ResourceException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * First class object representing the Registration request for the Secure Link protocol.  This 
+ * provides a toJson() utility to serialize information for over-the-wire communication and 
+ * provides a constructor to create from a serialized JSONObject.
+ * 
+ * @author rayc@google.com (Ray Colline)
+ */
+public class RegistrationRequest {
+
+  static final String RESOURCES_KEY = "resources";
+  
+  // Dependencies
+  private ResourceRuleUtil resourceRuleUtil;
+  
+  private JSONArray resourcesJsonArray;
+  private List<ResourceRule> resourceRules;
+  
+  
+  public RegistrationRequest() {
+    this(new ResourceRuleUtil());
+  }
+  
+  public RegistrationRequest(ResourceRuleUtil resourceRuleUtil) {
+    this.resourceRuleUtil = resourceRuleUtil;
+    resourceRules = new ArrayList<ResourceRule>();
+  }
+  
+  /**
+   * Creates the request from JSONObject.
+   * 
+   * @param json The JSON representing the object.
+   * @throws ResourceException if the JSON is malformed.
+   */
+  public void populateFromJSON(final JSONObject json) throws ResourceException {
+    try {
+      resourcesJsonArray = json.getJSONArray(RESOURCES_KEY);
+      for (int index=0; index < resourcesJsonArray.length(); index++) {
+        String resourceXml = resourcesJsonArray.getString(index);
+        resourceRules.add(resourceRuleUtil.getResourceRuleFromEntityXml(resourceXml));
+      }
+    } catch (JSONException e) {
+      throw new ResourceException(e);
+    }
+  }
+  
+  /**
+   * Creates empty object that must be populated before calling toJSON().
+   */
+  public void populateFromResources(List<ResourceRule> resourceRules) throws ResourceException {
+    resourcesJsonArray = new JSONArray();
+    for (ResourceRule resourceRule : resourceRules) {
+      resourcesJsonArray.put(resourceRuleUtil.getEntityXmlFromResourceRule(resourceRule));
+      this.resourceRules.add(resourceRule);
+    }
+  }
+
+  public List<ResourceRule> getResources() {
+    return resourceRules;
+  }
+  
+  /**
+   * Returns JSON object representing data.
+   * 
+   * @return populated JSON object with class data.
+   * @throws JSONException if any fields are null.
+   */
+  public JSONObject toJson() throws JSONException {
+    JSONObject json = new JSONObject();
+    json.put(RESOURCES_KEY, resourcesJsonArray);
+    return json;
+  }
+}
+  

02171e7dac95ee16162caeb97c5178d8363ccf8d
vasunori <vasunori@466b8f56-9b98-11dd-b1d9-6d7c01a26949>
2009-02-03 19:13:26 +0000

    change "name" in resourceRules to "seqNum". 1. allow only seqNUm > 0. 2. assign Integer.MAX_VALUE to the generated healthz rules 3. deprecate usage of "name" in resourcerules - direct the user to use seqNum

--- trunk/data-connector-agent/src/java/com/google/dataconnector/registration/v2/ResourceRule.java
+++ trunk/data-connector-agent/src/java/com/google/dataconnector/registration/v2/ResourceRule.java
@@ -29,20 +29,16 @@ public class ResourceRule implements Comparable<ResourceRule> {
   public static final String HTTPSID = "https://";
   public static final String SOCKETID = "socket://";
   
-  private String name;
+  private int seqNum;
+  // this exists for backward compatibility. clients with seqNum field will not have this field
+  private String name; 
   
   private String clientId;
-  
   private String[] allowedEntities;
-  
   private String[] appIds;
-  
   private String pattern;
-  
   private Integer httpProxyPort;
-  
   private Integer socksServerPort;
-  
   private Long secretKey;
   
   // getters and setters
@@ -52,6 +48,14 @@ public class ResourceRule implements Comparable<ResourceRule> {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public int getSeqNum() {
+    return seqNum;
+  }
+
+  public void setSeqNum(int seqNum) {
+    this.seqNum = seqNum;
   }
 
   public String getClientId() {
@@ -114,18 +118,12 @@ public class ResourceRule implements Comparable<ResourceRule> {
    * Compares two rules by sequence number.
    */
   public int compareTo(ResourceRule o) {
-    try {
-      int mySeqNumber = Integer.valueOf(name);
-      int theirSeqNumber = Integer.valueOf(name);
-      if (mySeqNumber < theirSeqNumber) {
-        return -1;
-      } else if (mySeqNumber > theirSeqNumber) {
-        return 1;
-      } else {
-        return 0;
-      }
-    } catch (NumberFormatException e) {
-      throw new RuntimeException(e);  
+    if (getSeqNum() < o.getSeqNum()) {
+      return -1;
+    } else if (getSeqNum() > o.getSeqNum()) {
+      return 1;
+    } else {
+      return 0;
     }
   }
 }

e1da0ca9f94727e9556f67699c456a40d3d7e3d7
darrell.kuhn <darrell.kuhn@466b8f56-9b98-11dd-b1d9-6d7c01a26949>
2009-02-20 00:25:39 +0000

    Updated clean target in build.xml-dist to include new files Added apache configure script for deb/rpm installs Added comment to localConfig.xml about permitopen option Added permitopen option to authorized_keys file vs cmdline to sshd Removed $* from start_sshd.sh-dist so cmdlin options are not passed Commented out PermitTunnel in sshd_config-dist, using permit open in authorized_keys Updated configure.sh to include new scripts for configure_apache and configure_sshdconf Updatad distribute/build.xml with changes that fix problems with build all Updated rpm/debian postinstall script to call apache and sshd config scripts Updated rpm to build rpm based on source tarball vs binary distro tarball

---
+++ changes/dkuhn/n4h2/trunk/data-connector-agent/src/java/com/google/dataconnector/util/ApacheSetupException.java
@@ -0,0 +1,53 @@
+/* Copyright 2008 Google Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.google.dataconnector.util;
+
+/**
+ * Apache HTTPD setup exceptions.
+ * 
+ * @author rayc@google.com (Ray Colline)
+ */
+public class ApacheSetupException extends AgentConfigurationException {
+
+  /**
+   * Creates the exception with the specified error message.
+   * 
+   * @param msg the error message.
+   */
+  public ApacheSetupException(String msg) {
+    super(msg);
+  }
+  
+  /**
+   * Creates the exception with the specified underlying cause.
+   * 
+   * @param cause the underlying cause.
+   */
+  public ApacheSetupException(Throwable cause) {
+    super(cause);
+  }
+  
+  /**
+   * Creates the exception with the specified error message and cause.
+   * 
+   * @param msg the error message.
+   * @param cause the underlying cause.
+   */
+  public ApacheSetupException(String msg, Throwable cause) {
+    super(msg, cause);
+  }
+}
